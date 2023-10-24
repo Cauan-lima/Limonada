6d4a4de7310cc57970e946155713b1c3b11d5f1a
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-10-24 16:04:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/limonada/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.limonada
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.limonada.ui.theme.LimonadaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LimonadaTheme {
+                 {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun limonada(){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.limoeiro),
+            contentDescription =null
+        )
+        Text(
+            text = stringResource(R.string.limoeiro)
+
+    }
+
+}
